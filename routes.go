c66358b514378118514c07c4ceb236b60298bb6c
SinistreCyborg <sinistrecyborg@yahoo.com>
2021-01-18 11:08:47 -0500

    feat(routes): Added routes to modify routes

--- routes.go
+++ routes.go
@@ -1,12 +1,20 @@
 package main
 
 import (
+	"io/ioutil"
 	"net/http"
 	"strings"
 )
 
 func handleRoutes(w http.ResponseWriter, r *http.Request) {
-	http.HandlerFunc(getRoute).ServeHTTP(w, r)
+	switch r.Method {
+	case "GET":
+		http.HandlerFunc(getRoute).ServeHTTP(w, r)
+	case "POST":
+		http.HandlerFunc(postRoute).ServeHTTP(w, r)
+	case "DELETE":
+		http.HandlerFunc(deleteRoute).ServeHTTP(w, r)
+	}
 }
 
 func getRoute(w http.ResponseWriter, r *http.Request) {
@@ -17,4 +25,23 @@ func getRoute(w http.ResponseWriter, r *http.Request) {
 	} else {
 		http.Error(w, http.StatusText(404), 404)
 	}
+}
+
+func postRoute(w http.ResponseWriter, r *http.Request) {
+	split := strings.Split(r.URL.Path, "/")
+
+	defer r.Body.Close()
+	bytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, http.StatusText(400), 400)
+		return
+	}
+
+	routes[split[len(split)-1]] = string(bytes)
+	w.Write([]byte("https://" + r.Host + "/" + split[len(split)-1] + "\n"))
+}
+
+func deleteRoute(w http.ResponseWriter, r *http.Request) {
+	split := strings.Split(r.URL.Path, "/")
+	delete(routes, split[len(split)-1])
 }
