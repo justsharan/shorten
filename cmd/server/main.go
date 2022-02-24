b4bbf37ed99e59016229e99a110939bb0ddc7a7a
Sharan Srinivasan <sharansr@student.ubc.ca>
2022-02-24 09:37:34 -0800

    Make port an int

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,18 +3,20 @@ package main
 import (
 	"flag"
 	"net/http"
+	"strconv"
 
 	"github.com/justsharan/shorten/internal/middleware"
 )
 
 var (
-	port = flag.String("port", "3000", "The port to listen for incoming requests")
+	port = flag.Int("port", 3000, "The port to listen for incoming requests")
 	auth = flag.String("auth", "password", "The Authorization header to look for")
+	store = flag.String("store", "routes.txt", "File to store routes in")
 )
 
 func main() {
 	handler := middleware.Logger(handleIncoming)
-	http.ListenAndServe(":" + *port, handler)
+	http.ListenAndServe(":" + strconv.Itoa(*port), handler)
 }
 
 func handleIncoming(w http.ResponseWriter, r *http.Request) {
